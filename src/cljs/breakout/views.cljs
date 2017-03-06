cf7c1d012f405084505029bc763ddc96be03de30
Arne Brasseur <arne@arnebrasseur.net>
2017-03-06 14:50:45 +0100

    Fixes so it matches the start of the episode

--- src/cljs/breakout/views.cljs
+++ src/cljs/breakout/views.cljs
@@ -1,21 +1,21 @@
 (ns breakout.views
-    (:require [re-frame.core :as re-frame]
-              [breakout.views.helpers :refer [background-box translate]]
-              [breakout.views.swatches :refer [color-swatch selection-marker]]
-              [breakout.game :as g]))
+  (:require [re-frame.core :as re-frame]
+            [breakout.views.helpers :refer [background-box translate]]
+            [breakout.views.swatches :refer [color-swatch selection-marker]]
+            [breakout.game :as g]))
 
 (defn color-picker []
   [:g
-   [background-box 0 0 60 310]
-   (for [idx (range 5)]
+   [background-box {:x 0 :y 0 :width 60 :height 310}]
+   (for [idx (range 6)]
      [color-swatch {:index idx
                     :key (str "color-" idx)}])
    [selection-marker {:index 0
                       :key "marker"}]])
 
 (defn block [{:keys [x y color]}]
-  [:rect {:x (g/x->px x)
-          :y (g/y->px y)
+  [:rect {:x (g/col->px x)
+          :y (g/row->px y)
           :width g/block-width
           :height g/block-height
           :class (g/color-class color)}])
@@ -33,7 +33,7 @@
   [:div.game
    [:h1 "BREAKOUT LEVEL EDITOR"]
    [:svg {:style {:width (+ g/width 70) :height g/height}}
-    [background-box 0 0 g/width g/height]
+    [background-box {:x 0 :y 0 :width g/width :height g/height}]
     [blocks]
     [translate {:x 1009 :y 0}
      [color-picker]]]])
--- src/cljs/breakout/views/helpers.cljs
+++ src/cljs/breakout/views/helpers.cljs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (ns breakout.views.helpers)
 
 (defn background-box
@@ -17,7 +16,6 @@
    [:g {:transform (str "translate(" (attrs :x) " " (attrs :y) ")")}]
    children))
 
-
 (defn click-pos
   "Get the position of a mouse event relative to the element's bounding box."
   [event]
@@ -28,23 +26,3 @@
         event-y (.-clientY event)]
     [(- event-x bounds-left)
      (- event-y bounds-top)]))
-||||||| parent of 2bcf9f4... Refactor to make the code easier to explain
-=======
-(ns breakout.views.helpers)
-
-(defn background-box
-  "Draw a grey box with black border, factoring in the border width"
-  [x y width height]
-  [:rect.fill--gray3.stroke--black {:x (inc x)
-                                    :y (inc y)
-                                    :width (- width 2)
-                                    :height (- height 2)
-                                    :key (str "box-" x "--" y)}])
-
-(defn translate
-  "Move a group of elements to a new location"
-  [attrs & children]
-  (into
-   [:g {:transform (str "translate(" (attrs :x) " " (attrs :y) ")")}]
-   children))
->>>>>>> 2bcf9f4... Refactor to make the code easier to explain
